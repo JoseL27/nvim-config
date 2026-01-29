d48d23d0b27c6fb6325a1bd4105e3fd1d93b40e2
Jose Luis Vallejo <jlv.vallejo27@gmail.com>
2026-01-29 18:54:45 +0100

    Moved keybinds to new file and changed options to be set in .vimrc

--- .vimrc
+++ .vimrc
@@ -1,8 +1,52 @@
 let mapleader=" "
 
+" Highlights matches when searching test
 set incsearch
+
+" Enable relative line numbers
 set relativenumber
-set scrolloff=5
+
+" Minimum lines that will be shown above and below the cursor
+set scrolloff=10
+
+" Before performing commands that would fail due to the file not being written,
+" show a prompt asking if you want to save the changes
+set confirm
+
+" Enable mouse mode
+set mouse=a
+
+" Don't show the mode, since it's already in the status line
+set noshowmode
+
+" Enable break indent
+set breakindent
+
+" Save undo history
+set undofile
+
+" Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+set ignorecase
+set smartcase
+
+" Keep signcolumn on by default
+set signcolumn=yes
+
+" Decrease update time
+set updatetime=250
+
+" Decrease mapped sequence wait time
+set timeoutlen=300
+
+" Configure how new splits should be opened
+set splitright
+set splitbelow
+
+" Preview substitutions live, as you type!
+set inccommand=split
+
+" Show which line your cursor is on
+set cursorline
 
 imap jk <Esc>
 nmap - $
---
+++ config/keybinds.lua
@@ -0,0 +1,42 @@
+-- Map leader is set in .vimrc
+
+-- Other keybinds not inherent to neovim are in .vimrc
+
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic Config & Keymaps
+-- See :help vim.diagnostic.Opts
+--
+-- NOTE: This probably needs to be moved to another file
+vim.diagnostic.config {
+  update_in_insert = false,
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+
+  -- Can switch between these as you prefer
+  virtual_text = true, -- Text shows up at the end of the line
+  virtual_lines = false, -- Teest shows up underneath the line, with virtual lines
+
+  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+  jump = { float = true },
+}
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is easier to discover
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Same keymap for exiting terminal mode as insert mode
+vim.keymap.set('t', 'jk', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
